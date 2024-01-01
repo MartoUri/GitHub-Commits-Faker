5f732dc15c9af70d542e56c0bf789c9b896e3b75
Martin <martin71211@gmail.com>
2024-01-01 00:00:00 -0300

    Gotta love commits!

--- OldCommits.py
+++ OldCommits.py
@@ -11,12 +11,15 @@ today = datetime.datetime.now().strftime("%m/%d/%Y")
 commit_date = datetime.datetime.fromisocalendar(int(today.split('/')[2]), 1, 1)
 days = (datetime.datetime.now() - datetime.datetime.strptime(CommitsFrom, "%m/%d/%Y")).days
 
+CommitsPerDay = [random.randint(MinCommits, MaxCommits) for _ in range(days)]
+SumCommits = sum(CommitsPerDay)
+
 print(days)
 for i in range(days):
-    CommitsPerDay = random.randint(MinCommits, MaxCommits)
-    for x in range(CommitsPerDay):
+    CommitsUntilNow = sum(CommitsPerDay[:i])
+    for x in range(CommitsPerDay[i]):
         subprocess.run(["git", "commit", "--allow-empty", "-m", "Gotta love commits!", "--date", commit_date.isoformat()])
-        print(f'Commit {i*x}/{days*CommitsPerDay}')
+        print(f'Commit {CommitsUntilNow + x}/{SumCommits}')
     commit_date += datetime.timedelta(days=1)
 
 print('Pushing...')
