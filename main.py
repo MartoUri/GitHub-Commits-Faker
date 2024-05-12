4e9e23cab9e7ee7668617e8fda658df235b3f293
Martin <martin71211@gmail.com>
2024-05-11 21:39:42 -0300

    .

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 import time
 
 commits = 3 # Number of commits you would like to make
+TimeBetweenCommits = 1 # Time between each commit in seconds
 
 repo = Repo(os.getcwd())
 file = open("file.txt", "a")
@@ -14,7 +15,7 @@ def commit():
 for i in range(commits-1):
     print(f'Commit {i+1}/{commits}')
     commit()
-    time.sleep(1)
+    time.sleep(TimeBetweenCommits)
 
 file.close()
 
