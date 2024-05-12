e317beab00493d48c02878ff1fa37a962c7106b5
Martin <martin71211@gmail.com>
2024-05-11 21:36:38 -0300

    .

--- main.py
+++ main.py
@@ -1,11 +1,32 @@
 from git.repo import Repo
 import os
+import time
+
+commits = 3 # Number of commits you would like to make
+
 repo = Repo(os.getcwd())
-file = open("file.txt", "r+")
+file = open("file.txt", "a")
+
 def commit():
     file.write('!')
     repo.index.commit('Gotta love commits!')
-    origin = repo.remotes[0]
-    origin.push()
-commit()
+
+for i in range(commits-1):
+    print(f'Commit {i+1}/{commits}')
+    commit()
+    time.sleep(1)
+
 file.close()
+
+file = open("file.txt", "w")
+file.write('Commits!')
+file.close()
+
+repo.index.commit('Gotta love commits!')
+print(f'Commit {commits}/{commits}')
+
+print('Pushing...')
+origin = repo.remotes[0]
+print('Pushed!')
+origin.push()
+
